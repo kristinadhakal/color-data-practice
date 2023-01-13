6910c2bc0452d84cfb2db20292763bd85845c36d
kristinadhakal <112728281+kristinadhakal@users.noreply.github.com>
2023-01-13 10:46:07 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,59 @@
+// WEB COLORS START
+
+// HTML Variable for Output
+let outputEl = document.getElementById("output");
+
+// Load Color Data
+let colorData;
+
+fetch("color-data.json")
+  .then((rawData) => rawData.json())
+  .then((data) => (colorData = data));
+
+// Event Listener on Go Button
+document.getElementById("go-btn").addEventListener("click", goBtnClicked);
+
+// Process Go Button Click
+function goBtnClicked() {
+  // Get Menu Selection
+  let selection = document.getElementById("menu-select").value;
+
+  // Process Menu Selection
+  if (selection === "all-colors") {
+    allColors();
+  } else if (selection === "bright-colors") {
+    brightColors();
+  } else if (selection === "red-pink-families") {
+    redPinkFamilies();
+  } else if (selection === "family-search") {
+    familySearch();
+  } else if (selection === "start-letter-search") {
+    startLetterSearch();
+  }
+}
+
+// MENU FUNCTIONS
+function allColors() {
+  // Display Name and Family of All Colors
+  outputEl.innerHTML = "<h3>Display All Colors</h3>";
+}
+
+function brightColors() {
+  // Display Name and Brightness of All Colors with a Brightness of 200 and Higher
+  outputEl.innerHTML = "<h3>Display Bright Colors</h3>";
+}
+
+function redPinkFamilies() {
+  // Count Colors in Red/Pink Families
+  outputEl.innerHTML = "<h3>Count Red/Pink Family Colors</h3>";
+}
+
+function familySearch() {
+  // Display Name and Family of all Colors that Match a User Provided Family Name. Also Output a Count of Colors Found.
+  outputEl.innerHTML = "<h3>Family Search</h3>";
+}
+
+function startLetterSearch() {
+  // Display Name of all Colors that Match a User Provided Starting Letter. Also Output a Count of Colors Found.
+  outputEl.innerHTML = "<h3>Start Letter Search</h3>";
+}
